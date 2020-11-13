e59e8f320420c287304c0ba1903ccaa212ee9f52
RJ Mattingly <mattingly.rj@gmail.com>
2020-11-12 16:16:22 -0800

    Add unit test for all methods. Execute still needs more

--- MaggiLizer/MaggiLizerTests/MaggiLizerTests.cpp
+++ MaggiLizer/MaggiLizerTests/MaggiLizerTests.cpp
@@ -21,9 +21,9 @@ namespace MaggiLizerTests
 
 		/// Helpers
 		// 
-		void FillBufferSingleWithData(buffer_single pBuffer, const float pattern[], uint patternSize, uint repeats)
+		buffer_single FillBufferSingleWithData(const float pattern[], uint patternSize, uint repeats = 1)
 		{
-			pBuffer = new float[(long long) patternSize * (long long) repeats];
+			buffer_single pBuffer = new float[(long long) patternSize * (long long)repeats];
 
 			for (uint i = 0; i < repeats; i++)
 			{
@@ -31,18 +31,23 @@ namespace MaggiLizerTests
 				{
 					uint pos = patternSize * i + j;
 					pBuffer[pos] = pattern[j];
+					pBuffer[pos] = pBuffer[pos];
+
 				}
 			}
+			return pBuffer;
 		}
 
-		void FillBufferWithData(buffer pBuffer, uint channels, const float pattern[], uint patternSize, uint repeats)
+		buffer FillBufferWithData(uint channels, const float pattern[], uint patternSize, uint repeats = 1)
 		{
-			pBuffer = new buffer_single[channels];
-
+			buffer pBuffer = new buffer_single[channels];
+			
 			for (uint i = 0; i < channels; i++)
 			{
-				FillBufferSingleWithData(pBuffer[i], pattern, patternSize, repeats);
+				pBuffer[i] = FillBufferSingleWithData(pattern, patternSize, repeats);
+				//FillBufferSingleWithData(pBuffer[i], pattern, patternSize, repeats);
 			}
+			return pBuffer;
 		}
 
 		void AssertBufferSingleValues(const float expected[], buffer_single actual, uint size)
@@ -52,6 +57,15 @@ namespace MaggiLizerTests
 				Assert::AreEqual(expected[i], actual[i], assert_tolerance);
 			}
 		}
+
+		void AssertBufferSingleValues(buffer_single expected, buffer_single actual, uint size)
+		{
+			for (uint i = 0; i < size; i++)
+			{
+				Assert::AreEqual(expected[i], actual[i], assert_tolerance);
+			}
+		}
+
 		//
 		/// \Helpers
 
@@ -61,8 +75,7 @@ namespace MaggiLizerTests
 		{
 			float expected[9] = { 0,1,2,0,1,2,0,1,2 };
 
-			buffer_single pBuffer;
-			FillBufferSingleWithData(pBuffer, new float[] { 0, 1, 2 }, 3, 3);
+			buffer_single pBuffer = FillBufferSingleWithData(expected, 9);
 
 			AssertBufferSingleValues(expected, pBuffer, 9);
 		}
@@ -75,9 +88,8 @@ namespace MaggiLizerTests
 
 			float expected[channels][patternSize] = { { 0,1,2,0,1,2,0,1,2 } , { 0,1,2,0,1,2,0,1,2 } };
 
-			buffer pBuffer;
-			FillBufferWithData(pBuffer, channels, pattern, patternSize, 1);
-
+			buffer pBuffer = FillBufferWithData(channels, pattern, 9);
+			
 			for (uint i = 0; i < channels; i++)
 			{
 				AssertBufferSingleValues(pattern, pBuffer[i], patternSize);
@@ -88,61 +100,238 @@ namespace MaggiLizerTests
 
 		/// Method Tests
 		//
-
-		
 		TEST_METHOD(Method_SetBufferValue)
 		{
-			Assert::Fail();
+			MaggiLizerFXDSP* dsp = new MaggiLizerFXDSP;
+
+			uint channels = 2;
+
+			buffer pBuffer = FillBufferWithData(channels, new float[] {0}, 1, 10);
+
+			uint channelIndex = 1;
+			uint bufferSamplePosition = 7;
+			float value = 0.021;
+
+			dsp->SetBufferValue(pBuffer, channelIndex, bufferSamplePosition, value);
+		
+			Assert::AreEqual(pBuffer[channelIndex][bufferSamplePosition], value);
 		}
 
 		TEST_METHOD(Method_ClearBufferSingle)
 		{
-			Assert::Fail();
+
+			float expected[] = { 0,0,0,0,0,0,0,0,0,0 };
+			
+			// SETUP
+			uint bufferSize = 10;
+
+			MaggiLizerFXDSP* dsp = new MaggiLizerFXDSP;
+
+			buffer_single pBuffer = FillBufferSingleWithData(new float[] {0.1}, 1, bufferSize);
+
+			// TEST
+			dsp->ClearBufferSingle(pBuffer, bufferSize);
+
+			// VALIDATE
+			AssertBufferSingleValues(expected, pBuffer, bufferSize);
 		}
 
-		TEST_METHOD(Method_SwapBufferValues)
+		TEST_METHOD(Method_SwapBufferValues_EvenSizeBuffer)
 		{
-			Assert::Fail();
+			float input[] = {0, 1, 2, 3, 4, 5, 6, 7};
+			float expected[]{ 0, 1, 5, 3, 4, 2, 6, 7 };
+
+			//SETUP
+			uint leftPos = 2;
+			uint rightPos = 5;
+			uint bufferSize = 8;
+
+			MaggiLizerFXDSP* dsp = new MaggiLizerFXDSP;
+
+			buffer_single pBuffer = FillBufferSingleWithData(input, bufferSize);
+
+			//TEST
+			dsp->SwapBufferValues(&pBuffer[leftPos], &pBuffer[rightPos]);
+
+			//VALIDATE
+			AssertBufferSingleValues(expected, pBuffer, bufferSize);
 		}
 
-		TEST_METHOD(Method_ReverseBuffer)
+		TEST_METHOD(Method_SwapBufferValues_OddSizeBuffer)
 		{
-			Assert::Fail();
+			float input[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+			float expected[]{ 0, 1, 6, 3, 4, 5, 2, 7, 8 };
+
+			//SETUP
+			uint leftPos = 2;
+			uint rightPos = 6;
+			uint bufferSize = 9;
+
+			MaggiLizerFXDSP* dsp = new MaggiLizerFXDSP;
+
+			buffer_single pBuffer = FillBufferSingleWithData(input, bufferSize);
+
+			//TEST
+			dsp->SwapBufferValues(&pBuffer[leftPos], &pBuffer[rightPos]);
+
+			//VALIDATE
+			AssertBufferSingleValues(expected, pBuffer, bufferSize);
 		}
 
 		TEST_METHOD(Method_ApplyReverseBufferSingle)
 		{
-			Assert::Fail();
+			float input[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+			float expected[] = { 8, 7, 6, 5, 4, 3, 2, 1, 0};
+
+			//SETUP
+			uint bufferSize = 9;
+			bool reverse = true;
+
+			MaggiLizerFXDSP* dsp = new MaggiLizerFXDSP;
+
+			buffer_single pBuffer = FillBufferSingleWithData(input, bufferSize);
+			
+			//TEST
+			dsp->ApplyReverseBufferSingle(pBuffer, bufferSize, reverse);
+
+			//VALIDATE
+			AssertBufferSingleValues(expected, pBuffer, bufferSize);
 		}
 
-		TEST_METHOD(Method_ApplySpeedBufferSingle)
+		TEST_METHOD(Method_ApplySpeedBufferSingle_Double)
 		{
-			Assert::Fail();
+			float input[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+			float expected[] = { 0, 2, 4, 6, 8, 0, 0, 0, 0, 0};
+			
+			// SETUP
+			uint bufferSize = 10;
+			float speed = 2.0;
+
+			MaggiLizerFXDSP* dsp = new MaggiLizerFXDSP;
+
+			buffer_single in_pBuffer = FillBufferSingleWithData(input, bufferSize);
+			buffer_single out_pBuffer = FillBufferSingleWithData(new float{ 0 }, 1, bufferSize);
+
+			//TEST
+			dsp->ApplySpeedBufferSingle(in_pBuffer, out_pBuffer, bufferSize, speed);
+
+			//VALIDATE
+			AssertBufferSingleValues(expected, out_pBuffer, bufferSize);
+		}
+
+		TEST_METHOD(Method_ApplySpeedBufferSingle_Half)
+		{
+			float input[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+			float expected[] = { 0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5 };
+
+			// SETUP
+			uint bufferSize = 10;
+			float speed = 0.5;
+
+			MaggiLizerFXDSP* dsp = new MaggiLizerFXDSP;
+
+			buffer_single in_pBuffer = FillBufferSingleWithData(input, bufferSize);
+			buffer_single out_pBuffer = FillBufferSingleWithData(new float{ 0 }, 1, bufferSize);
+
+			//TEST
+			dsp->ApplySpeedBufferSingle(in_pBuffer, out_pBuffer, bufferSize, speed);
+
+			//VALIDATE
+			AssertBufferSingleValues(expected, out_pBuffer, bufferSize);
 		}
 
 		TEST_METHOD(Method_CaclulateBufferSampleSize)
 		{
-			Assert::Fail();
+			uint expected = 76800;
+			uint bufferSampleSize = 0; //to be set by test
+
+			//SETUP
+			uint sampleRate = 48000;
+			float splice = 1600; //ms
+			MaggiLizerFXDSP* dsp = new MaggiLizerFXDSP;
+
+			//TEST
+			bufferSampleSize = dsp->CaclulateBufferSampleSize(sampleRate, splice);
+
+			//VALIDATE
+			Assert::AreEqual(expected, bufferSampleSize);
 		}
 
 		TEST_METHOD(Method_CalculateBufferSizeChangeFromSpeed)
 		{
-			Assert::Fail();
+			uint expected = 48000;
+			uint newBufferSpeed = 0; //to be set by test
+
+			//SETUP
+			uint bufferSize = 96000;
+			float speed = 2;
+
+			MaggiLizerFXDSP* dsp = new MaggiLizerFXDSP;
+
+			//TEST
+			newBufferSpeed = dsp->CalculateBufferSizeChangeFromSpeed(bufferSize, speed);
+
+			//VALIDATE
+			Assert::AreEqual(expected, newBufferSpeed);
 		}
 
 		TEST_METHOD(Method_GetBufferValue)
 		{
-			Assert::Fail();
+			float input[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+			float expected = 4;
+			float outputValue = -1; //to be set by test
+
+			//SETUP
+			uint channels = 2;
+			uint bufferSize = 10;
+			uint channelIndex = 0;
+			uint bufferPosition = 4;
+
+			MaggiLizerFXDSP* dsp = new MaggiLizerFXDSP;
+
+			buffer pBuffer = FillBufferWithData(channels, input, bufferSize);
+
+			//TEST
+			outputValue = dsp->GetBufferValue(pBuffer, channelIndex, bufferPosition);
+
+			//VALIDATE
+			Assert::AreEqual(expected, outputValue);
 		}
 
 		TEST_METHOD(Method_CalculateWetDryMix)
 		{
-			Assert::Fail();
+			float expected = 0.5;
+			float mix = 0; //to be set by test
+
+			//SETUP
+			float input = 1;
+			float output = 0;
+			float mixPercent = 0.5;
+			MaggiLizerFXDSP* dsp = new MaggiLizerFXDSP;
+
+			//TEST
+			mix = dsp->CalculateWetDryMix(input, output, mixPercent);
+
+			//VALIDATE
+			Assert::AreEqual(expected, mix);
+
 		}
 
 		TEST_METHOD(Method_CalculateSpeed)
 		{
-			Assert::Fail();
+			float expected = 0.5;
+			float speed = 0;
+
+			//SETUP
+			float pitch = -1200;
+
+			MaggiLizerFXDSP* dsp = new MaggiLizerFXDSP;
+
+			//TEST
+			speed = dsp->CalculateSpeed(pitch);
+
+			//VALIDATE
+			Assert::AreEqual(expected, speed);
 		}
 
 
@@ -151,9 +340,10 @@ namespace MaggiLizerTests
 		/// When the plugin first starts receiving samples, it has not yet filled it's playback buffer.
 		/// While it is filling it's uinternal buffer for playback, it outputs silence (0).
 		/// </summary>
-		TEST_METHOD(SetsInitialBufferToZero)
+		TEST_METHOD(Execute_SetsInitialBufferToZero)
 		{
 			float expected[] = { 0,0,0,0,0,0,0,0,0,0 };
+			float input[] = { 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1 };
 
 			uint sampleRate = 48000;
 			uint channels = 1;
@@ -165,8 +355,7 @@ namespace MaggiLizerTests
 			float recycle = 0;
 			float mix = 1;
 
-			buffer pBuffer = new float* [1];
-			FillBufferWithData(pBuffer, channels, new float[] {0.1}, 1, bufferBlockSize);
+			buffer pBuffer = FillBufferWithData(channels, input, bufferBlockSize);
 
 			MaggiLizerFXDSP* p_DSP = new MaggiLizerFXDSP();
 			p_DSP->Init(sampleRate, splice, channels);
@@ -181,7 +370,7 @@ namespace MaggiLizerTests
 		/// Description:
 		/// If Mix value is set to 0, input buffer should match output buffer
 		/// </summary>
-		TEST_METHOD(MixOfZeroDoesNotModifyBuffer)
+		TEST_METHOD(Execute_MixOfZeroDoesNotModifyBuffer)
 		{
 			float expected[] = { 1,2,3,1,2,3,1,2,3,1,2,3,1,2,3 };
 
@@ -195,9 +384,7 @@ namespace MaggiLizerTests
 			float recycle = 0;
 			float mix = 0;
 
-			buffer pBuffer = new float* [1];
-			pBuffer[0] = new float[bufferBlockSize];
-			FillBufferSingleWithData(pBuffer[0], new float[] {1, 2, 3}, 3, 5);
+			buffer pBuffer = FillBufferWithData(channels, new float[]{1, 2, 3}, 3, 5);
 			
 			MaggiLizerFXDSP* p_DSP = new MaggiLizerFXDSP();
 			p_DSP->Init(sampleRate, splice, channels);
